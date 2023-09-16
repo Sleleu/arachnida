51b29bd908d276d7654bddd2a364997198da7586
sleleu <leleu.sebastien@hotmail.fr>
2023-09-16 20:55:24 +0200

    add extension check + ascii header

--- spider.py
+++ spider.py
@@ -6,6 +6,22 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 import time
 
+ascii_header = """
+  ██████  ██▓███   ██▓▓█████▄ ▓█████  ██▀███  
+▒██    ▒ ▓██░  ██▒▓██▒▒██▀ ██▌▓█   ▀ ▓██ ▒ ██▒
+░ ▓██▄   ▓██░ ██▓▒▒██▒░██   █▌▒███   ▓██ ░▄█ ▒
+  ▒   ██▒▒██▄█▓▒ ▒░██░░▓█▄   ▌▒▓█  ▄ ▒██▀▀█▄  
+▒██████▒▒▒██▒ ░  ░░██░░▒████▓ ░▒████▒░██▓ ▒██▒
+▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░▓   ▒▒▓  ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░
+░ ░▒  ░ ░░▒ ░      ▒ ░ ░ ▒  ▒  ░ ░  ░  ░▒ ░ ▒░
+░  ░  ░  ░░        ▒ ░ ░ ░  ░    ░     ░░   ░ 
+      ░            ░     ░       ░  ░   ░     
+                       ░                      
+			Created by : https://github.com/Sleleu
+"""
+
+g_downloaded_files = set()
+
 def getResponse(url: str):
 	try:
 		response = requests.get(url)
@@ -21,22 +37,38 @@ def getImgSrc(images, url):
 		img_src.append(requests.compat.urljoin(url, src)) # in case of non absolute image url in htmldata
 	return(img_src)
 
-def isValidExtension(img_response):
+def isValidExtension(img_response): # check for mime type of the content
 	valid_mime_types = ["image/jpg", "image/jpeg", "image/png", "image/bmp", "image/gif"]
 	content_type = img_response.headers.get('content-type')
 	if content_type in valid_mime_types:
 		return (True)
 	return (False)
 
+def isImageElement(img_path): # check for extension of the image path
+	valid_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif"]
+	for extension in valid_extensions:
+		if img_path.endswith(extension):
+			return (True)
+	return (False)
+
 def downloadImage(image_url, path):
+	if image_url in g_downloaded_files:
+		return
 	img_response = requests.head(image_url)
 	if isValidExtension(img_response):
-		img_content = requests.get(image_url).content
+		img_response = requests.get(image_url)
+		if (img_response.status_code != 200):
+			print(f"Cannot download {image_url}")
+			return
+		img_content = img_response.content
 		img_name = os.path.basename(urlparse(image_url).path)  # To avoid url params
 		img_path = os.path.join(path, img_name)
-		if os.path.isdir(img_path):
+		if not isImageElement(img_path) or os.path.isdir(img_path):
 			return
-		open(img_path, 'wb').write(img_content)  # write in binary the content of the image
+		with open(img_path, 'wb') as img_file:  # write in binary the content of the image
+			img_file.write(img_content)
+		print_green(f"DOWNLOAD : {image_url}")
+		g_downloaded_files.add(image_url)
 
 def spider(url: str, path: str, depth_level: int):
 	if (depth_level == 0):
@@ -77,13 +109,17 @@ def getPath(path):
 	else:
 		return (path + '/')
 
+def print_green(str):
+	print("\033[32m" + str + "\033[0m")
+
 if __name__ == "__main__":
+	print_green(ascii_header)
 	start_time = time.time()
 
 	args = parse_arguments()
 	url = args.URL
 	path = getPath(args.path)
-	depth_level = 2
+	depth_level = 3
 	if not os.path.exists(path): # create the directory path if it does not exist
 		os.makedirs(path)
 	spider(url, path, depth_level)
